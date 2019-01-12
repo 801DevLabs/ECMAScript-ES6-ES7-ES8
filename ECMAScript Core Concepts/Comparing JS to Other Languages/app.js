40aa5ca633937c4ba3bf7758d041e417e3a422ec
801DevLabs <801DevLabs@gmail.com>
2019-01-12 12:51:10 -0700

    initial commit

---
+++ ECMAScript Core Concepts/Comparing JS to Other Languages/app.js
@@ -0,0 +1,49 @@
+/*
+============================================
+COMPARING JAVASCRIPT TO OTHER LANGUAGES
+============================================
+
+
+========================
+PYTHON LANGUAGE
+========================
+*/
+
+let i = 1000;
+
+for(let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+console.log(i)
+
+/*
+========================
+PYTHON LANGUAGE
+========================
+
+i = 2;
+print i
+
+for i in range(0,10):
+  print "is is: ", i
+print "after all that i is: ",i
+
+========================
+RUBY LANGUAGE
+========================
+i = 2;
+puts i
+
+========================
+C LANGUAGE
+========================
+#include "stdio.h"
+
+int main(void) {
+  for(int i = 0; i < 10; i++) {
+    printf("value of i: %d/n",i);
+  }
+  return 0;
+}
+*/
---
+++ README.md
@@ -0,0 +1,1 @@
+# ECMAScript-ES6-ES7-ES8
